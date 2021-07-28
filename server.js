b783d5728f06d7e42867ddb934b04305c37f41d9
Ray Hiramatsu <ray@Rays-MacBook-Pro.local>
2021-07-28 09:24:15 -0500

    heroku setup (env file, dependencies)

--- server.js
+++ server.js
@@ -3,18 +3,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const db = mongoose.connection;
+// require('dotenv').config();
 
 //---------- Configuration ----------//
 const app = express();
-const PORT = 7777;
+const PORT = process.env.PORT || 7777;
+// const PORT = 7777;
 //---------- Middleware ----------//
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 //---------- Database ----------//
+const MONGODB_URI = process.env.MONGODB_URI;
+
 mongoose.connect(
-    'mongodb://localhost:27017/forum',
+    MONGODB_URI,
+    // "mongodb://localhost:27017/forum",
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -31,7 +36,10 @@ const forumController = require('./controllers/forum_controller.js');
 app.use('/', forumController);
 
 
-
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
 //---------- Routes ----------//
 
 // app.get('/', (req, res)=>{
