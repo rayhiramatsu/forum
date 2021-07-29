5735ba80242aa389706ad95439a1efb2f26851f3
Ray Hiramatsu <ray@Rays-MacBook-Pro.local>
2021-07-29 15:39:49 -0500

    more bulma

--- scratchpad.js
+++ scratchpad.js
@@ -8,3 +8,59 @@
 
 ///////////////////////////////////////////
 # MONGODBURI=mongodb://localhost:27017/forum
+
+
+/////////////////////////////////////////////
+For loop that prints posts for index
+
+<% for(let i = post.length - 1; i >= 0; i--){%>
+
+    <div class="thread">
+        <a href="/<%=post[i]._id%>">
+            <h3><%=post[i].title%></h3>
+        </a>
+        <p>Likes: <%=post[i].votes%></p>
+        <p><%=post[i]%></p>
+        <br/>
+    </div>
+<% } %>
+
+//////////////////////////////////////////////
+bulma class card
+
+<div class="card">
+    <!-- <div class="columns">
+
+    </div> -->
+    <div class="card-content">
+        <p class="title"><%=post[i].title%></p>
+
+    </div>
+
+</div>
+
+/////////////////////////////////////////////
+bulma card with columns
+
+<div class="card">
+    <div class="columns" style="border-radius: 5px">
+        <div class="column is-one-fifth has-background-warning">
+            <p><%=post[i].votes%></p>
+        </div>
+        <div class="column has-background-danger">
+            <div class="card-content">
+                <p class="title"><%=post[i].title%></p>
+            </div>
+        </div>
+    </div>
+    <div class="card-footer">
+        <div class="card-footer-item">
+            <a href="/<%=post[i]._id%>">num comments</a>
+        </div>
+    </div>
+
+</div>
+
+/////////////////////////////////////////////////
+
+# MONGODB_URI=mongodb+srv://comet:comet7@forum.s4htd.mongodb.net/forummongodb?retryWrites=true&w=majority
